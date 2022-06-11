dbd569256b7c4188e3f9df703037e4e8a16f292b
Jim Pick <jim@jimpick.com>
2022-06-10 19:04:17 -0700

    Puts some names into a hamt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use fvm_ipld_blockstore::MemoryBlockstore;
 use fvm_ipld_hamt::Hamt;
 
+// https://docs.rs/fvm_ipld_hamt/0.5.1/fvm_ipld_hamt/
+// https://github.com/Schwartz10/sample-erc20-fvm-actor/blob/43acb18d9509859d9adc1d50e1fed2d2cf6f023e/src/lib.rs#L76
+
 fn main() {
     println!("Hello, world!");
+    let store = MemoryBlockstore::default();
+    let mut names : Hamt<_, String, usize> = Hamt::new(&store);
+    names.set(1, "Jim".to_string()).unwrap();
+    println!("1: {}", names.get(&1).unwrap().unwrap());
+    names.set(2, "Sheldon".to_string()).unwrap();
+    println!("2: {}", names.get(&2).unwrap().unwrap());
 }
 
 #[test]
